c3c7c938054f406465b948dd576a0abcd07792ea
Ludek NOVAK <ludek.novak@colas.cz>
2020-03-12 22:03:29 +0100

    Update windows prompt.

--- scripts/install-windows.bat
+++ scripts/install-windows.bat
@@ -1,15 +1,16 @@
 @ECHO OFF
 
-mklink /J C:\Users\novakl1\.vim C:\Users\novakl1\vimfiles
+mklink /J %HOMEDRIVE%%HOMEPATH%\.vim %HOMEDRIVE%%HOMEPATH%\vimfiles
 
-mklink /J C:\Users\novakl1\vimfiles\.config C:\Users\novakl1\.config
+mklink /J %HOMEDRIVE%%HOMEPATH%\vimfiles\.config %HOMEDRIVE%%HOMEPATH%\.config
+mklink /H %HOMEDRIVE%%HOMEPATH%\vimfiles\.config\git\git-prompt.sh  %HOMEDRIVE%%HOMEPATH%\vimfiles\.prompt.sh
 
-mklink /H C:\Users\novakl1\.bash_history  C:\Users\novakl1\vimfiles\.bash_history
-mklink /H C:\Users\novakl1\.bash_profile  C:\Users\novakl1\vimfiles\.bash_profile
-mklink /H C:\Users\novakl1\.bashrc  C:\Users\novakl1\vimfiles\.bashrc
-mklink /H C:\Users\novakl1\.gitconfig  C:\Users\novakl1\vimfiles\.gitconfig
-mklink /H C:\Users\novakl1\.viminfo  C:\Users\novakl1\vimfiles\.viminfo
-mklink /H C:\Users\novakl1\.vimrc  C:\Users\novakl1\vimfiles\.vimrc
-mklink /H C:\Users\novakl1\.minttyrc  C:\Users\novakl1\vimfiles\.minttyrc
-mklink /H C:\Users\novakl1\_viminfo  C:\Users\novakl1\vimfiles\_viminfo
+mklink /H %HOMEDRIVE%%HOMEPATH%\.bash_history  %HOMEDRIVE%%HOMEPATH%\vimfiles\.bash_history
+mklink /H %HOMEDRIVE%%HOMEPATH%\.bash_profile  %HOMEDRIVE%%HOMEPATH%\vimfiles\.bash_profile
+mklink /H %HOMEDRIVE%%HOMEPATH%\.bashrc  %HOMEDRIVE%%HOMEPATH%\vimfiles\.bashrc
+mklink /H %HOMEDRIVE%%HOMEPATH%\.gitconfig  %HOMEDRIVE%%HOMEPATH%\vimfiles\.gitconfig
+mklink /H %HOMEDRIVE%%HOMEPATH%\.viminfo  %HOMEDRIVE%%HOMEPATH%\vimfiles\.viminfo
+mklink /H %HOMEDRIVE%%HOMEPATH%\.vimrc  %HOMEDRIVE%%HOMEPATH%\vimfiles\.vimrc
+mklink /H %HOMEDRIVE%%HOMEPATH%\.minttyrc  %HOMEDRIVE%%HOMEPATH%\vimfiles\.minttyrc
+mklink /H %HOMEDRIVE%%HOMEPATH%\_viminfo  %HOMEDRIVE%%HOMEPATH%\vimfiles\_viminfo
 
