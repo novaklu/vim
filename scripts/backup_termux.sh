183371086b94e5fadb31e9dcb9f74bcd667ca403
Ludek NOVAK <ludek.novak@colas.cz>
2020-03-12 12:33:21 +0100

    Update for Termux bash.

---
+++ scripts/backup_termux.sh
@@ -0,0 +1,9 @@
+# install script for termux
+# only copy file to home
+cp ~/.bashrc ~/vimfiles/.bashrc
+cp ~/.vimrc ~/vimfiles/.vimrc
+cp ~/.gitconfig ~/vimfiles/.gitconfig
+cp -r ~/.vim/spell ~/vimfiles/
+cp ~/.vim/README ~/vimfiles/README
+cp ~/.vim/README.md ~/vimfiles/README.md
+
